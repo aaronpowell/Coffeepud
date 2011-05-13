d3c97a3fff4a78095fcf5246c8878ff00e9876b0
aaronpowell <me@aaron-powell.com>
2011-05-13 13:10:45 +1000

    initial checking

---
+++ js/mylibs/coffeepud.js
@@ -0,0 +1,58 @@
+﻿script = 
+    act: 0
+    line: 0
+    acts: [
+        [
+            "Hello world",
+            "This is another line"
+        ],
+        [
+            "We're starting a new story",
+            "What do you want to talk about?"
+        ]
+    ]
+txt = $ 'textarea'
+running = false
+showScriptStep = () ->
+    return if script.act >= script.acts.length
+        
+    txt.val '' if script.line == 0
+    
+    act = script.acts[script.act]
+    line = act[script.line].split ''
+    line.reverse()
+    running = true
+    setTimeout go = () ->
+        l = line.pop()
+        if !l
+            running = false
+            l = '\r\n'
+        
+        txt.val txt.val() + l
+        
+        if running
+            setTimeout -> 
+                go()
+            , 50
+    , 50
+    
+    script.line++
+    if script.line >= act.length
+        script.act++
+        script.line = 0
+
+$ ->
+    txt.keypress (e) -> 
+        e.preventDefault
+
+        switch e.keyCode
+            when 13 
+                showScriptStep() if !running
+            else
+        return false
+    $(window).resize -> 
+        w = $ this
+        txt.height w.height() - 35
+        txt.width w.width() - 15
+        
+    $(window).resize()
---
+++ js/mylibs/site.js
@@ -0,0 +1,9 @@
+﻿$(function() {
+    $(window).resize(function() {
+        var $textarea = $('textarea');
+        var $window = $(this);
+        $textarea.height( $window.height() - 35 );
+        $textarea.width( $window.width() - 15 );
+    });
+    $(window).resize(); 
+});
